57ec0b88b765cf3510a734ce9c798288ffb23864
邢照珺 <542862986@qq.com>
2017-07-18 11:46:59 +0800

    EasyUI组件
    EasyUI前端页面框架添加

--- src/main/java/com/todd/framework/activemq/ConsumerMessage/ConsumerMessageListener.java
+++ src/main/java/com/todd/framework/activemq/ConsumerMessage/ConsumerMessageListener.java
@@ -6,10 +6,18 @@ import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alibaba.fastjson.JSONObject;
+import com.todd.framework.po.User;
+import com.todd.framework.service.IUserService;
 
 public class ConsumerMessageListener implements MessageListener {
 
 	private Logger log = Logger.getLogger(ConsumerMessageListener.class);
+
+	@Autowired
+	private IUserService userservice;
 
 	public void onMessage(Message message) {
 		System.out.println("开始休眠");
@@ -24,6 +32,11 @@ public class ConsumerMessageListener implements MessageListener {
 		TextMessage textMessage = (TextMessage) message;
 		try {
 			System.out.println("接收到的消息内容是：" + textMessage.getText());
+			JSONObject json = new JSONObject();
+			json = json.parseObject(textMessage.getText());
+			String name = json.getString("username");
+			User user = userservice.getUserWithName(name);
+			System.out.println(user.toString());
 			// TODO: 你喜欢的任何事情...
 		} catch (JMSException e) {
 			log.error("", e);
--- src/main/java/com/todd/framework/po/User.java
+++ src/main/java/com/todd/framework/po/User.java
@@ -80,4 +80,11 @@ public class User implements Serializable {
 	public void setRoles(List<Role> roles) {
 		this.roles = roles;
 	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", userName=" + userName + ", password=" + password + ", salt=" + salt + ", locked="
+				+ locked + "]";
+	}
+
 }
